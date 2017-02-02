d8a300eba258a56384d0cee8d8980b386ba2f466
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-02-03 00:15:41 +0100

    Recuperation de la liste des clients

--- kelio-bot.js
+++ kelio-bot.js
@@ -1,5 +1,5 @@
 var events = require('events');
-var Nightmare = require('nightmare');       
+var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
 var gateau = null;
 
@@ -9,27 +9,68 @@ module.exports = function(login, password, kelio_url) {
 
 	var that = this;
 	events.EventEmitter.call(this);
-	
+
+	this.kelio = function(){
+		return Nightmare({ show: false })
+		.goto(kelio_url)
+		.screenshot('screen/1_logginPage.png')
+		.type('#j_username', login)
+		.type('#j_password', password)
+		.click('#btnAction')
+		.wait(1000);
+	}
+
 	this.login = function(){
-		nightmare
-			.goto(kelio_url)
-			.type('#j_username', login)
-			.type('#j_password', password)
-			.click('#btnAction')
-			.screenshot('screen/loggedIn.png')
+		this.kelio()
+			.screenshot('screen/2_after_loggedIn.png')
 			.cookies.get()
 			.end()
 			.then(function(cookies) {
-				console.log(cookies);
 				gateau = cookies;
 				that.emit('connected');
-				done();
 			})
 			.catch(function(error) {
 				console.error(error);
 			});
 	}
-	
+
+	this.getGateau = function(){
+		return gateau;
+	}
+
+
+	this.getClients = function(){
+
+		this.kelio()
+			.wait(2000)
+			.screenshot('screen/3_afterCookiesSet.png')
+			.evaluate(() => {
+				var clients = [];
+				[...document.querySelectorAll('#b2')].map(client => {
+					clients.push(client.innerText.replace("&nbsp", ""));
+				});
+				return clients;
+			})
+			.end()
+			.then(function(clients) {
+				console.log("clients");
+				console.log(clients);
+				that.emit('clients',clients);
+			});
+	}
+
+	this.setClientValue = function(client){
+
+		this.kelio()
+			.wait(2000)
+			.evaluate(() => {
+				return
+			})
+			.end()
+			.then(function(clients) {
+				that.emit('clientSet',client);
+			});
+	}
 
 	return this;
 }
