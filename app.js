39a8f99d5830728303597512807671b8328abd9b
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-02-03 23:34:59 +0100

    Finish process client multiple

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (accounts && accounts.kelio && accounts.google) {
     //console.log(accounts);
 } else {
     console.warn('provide google / kelio accounts id in arguments or in config file');
-    process.exit()
+    process.exit();
 }
 
 var bot = new hangoutsBot(accounts.google.login,accounts.google.password);
@@ -49,19 +49,39 @@ bot.on('message', function(from, message) {
 kelio.on('clients', (clients) => {
     clients_list = clients;
     bot.sendMessage(config.to,"Ta liste de client j'ai trouvé !");
-    var message_clients = "";
-    for (var i = 0; i < clients_list.length; i++) {
-      message_clients += "- ("+i+")"+clients_list[i]+"\r\n";
-    }
-    bot.sendMessage(config.to,message_clients);
-    bot.sendMessage(config.to,"Ton client de la journée choisir tu dois.");
+    list_clients();
 })
+
+kelio.on('clientSet', (client) => {
+  bot.sendMessage(config.to,"Pourcentage pour '"+clients_list[client.client_id]+"' mis à jour !");
+  if(value_total == 100){
+    bot.sendMessage(config.to,"Cela fait 100% ! Bonne journée !");
+    bot.sendMessage(config.to,"Que la force soit avec toi !");
+    process.exit();
+  }else{
+    bot.sendMessage(config.to,"Cela ne fait pas 100%");
+    value_2_set = null;
+    list_clients();
+  }
+});
 
 kelio.on('connected', () => {
     console.log('Yoda connected to Kelio');
     bot.sendMessage(config.to,'Connecter je me suis !');
     kelio.getClients();
 });
+
+function list_clients(){
+  var message_clients = "";
+  for (var i = 0; i < clients_list.length; i++) {
+    message_clients += "- ("+i+")"+clients_list[i]+"\r\n";
+  }
+  bot.sendMessage(config.to,message_clients);
+  if(value_total > 0)
+    bot.sendMessage(config.to,"Ton autre client de la journée choisir tu dois.");
+  else
+    bot.sendMessage(config.to,"Ton client de la journée choisir tu dois.");
+}
 
 function switch_message(message){
   switch (message) {
@@ -70,6 +90,11 @@ function switch_message(message){
       case '2':
       case '3':
       case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
           value_2_set = {};
           value_2_set["client_id"] = message;
           value_2_set["value"] = null;
